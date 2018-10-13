e56f038eaf3d6fb015ce5efef974c72945972ba1
Oleg <oleg.syniakevych@idagames.com>
2018-10-13 18:27:13 +0300

    Initial Commit

---
+++ Dynamic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dynamic
+//
+//  Created by Oleg Syniakevych on 13/10/2018.
+//  Copyright © 2018 Oleg Syniakevych. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
