82779c84fa3d75fb07484ad3814bdb640906742d
Oleg <oleg.syniakevych@idagames.com>
2018-10-14 22:21:29 +0300

    clean solution

--- palindromic_partitioning/main.cpp
+++ palindromic_partitioning/main.cpp
@@ -9,9 +9,6 @@ char str[MaxStringLength + 1];
 size_t strLen;
 using cut_t = int;
 cut_t const MaxPossibleCuts = 1000;
-using index_t = char const *;
-
-enum class Research: char { unchecked=-1, no=0, yes=1 };
 
 bool palindrome[MaxStringLength][MaxStringLength];
 
@@ -31,37 +28,6 @@ void performTabulationForPalindromeTable() {
             size_t const end = start + offset;
             palindrome[start][end] = (str[start] == str[end]) and palindrome[start + 1][end - 1];
         }
-    }
-}
-
-cut_t cutsTable[MaxStringLength][MaxStringLength];
-
-void performTabulationForNumberOfCuts() {
-    for (size_t start = 0; start < strLen; ++start) {
-        size_t const offset = 0;
-        cutsTable[offset][start] = 0; // we need zero cuts for strings of length 1
-    }
-    
-    for (size_t offset = 0; offset < strLen; ++offset) {
-        size_t const start = 0;
-        size_t const end = start + offset;
-        cut_t res = -1;
-        if (palindrome[start][end]) {
-            res = 0;
-        } else {
-            cut_t minCuts = MaxPossibleCuts;
-            for (size_t i = start; i < end; ++i) {
-                size_t const start1 = start;
-                size_t const offset1 = i - start1;
-                size_t const start2 = i + 1;
-                size_t const offset2 = end - start2;
-                cut_t currCuts = cutsTable[offset1][start1] + 1 + cutsTable[offset2][start2];
-                if (minCuts > currCuts) minCuts = currCuts;
-            }
-            res = minCuts;
-        }
-        
-        cutsTable[offset][start] = res;
     }
 }
 
